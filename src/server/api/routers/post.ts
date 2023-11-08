541865b5386f64335d55c30ba7c15e3d647cb303
Daniel Cluff <58708714+danielcluff@users.noreply.github.com>
2023-11-08 09:40:58 -0700

    backend author fix

--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -1,10 +1,56 @@
+import { clerkClient } from "@clerk/nextjs";
+import { User } from "@clerk/nextjs/dist/types/server";
+import { TRPCError } from "@trpc/server";
 import { z } from "zod";
+import type { Post } from "@prisma/client";
 
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+import { filterUserForClient } from "../helpers/filterUserForClient";
+
+const addUserDataToPosts = async (posts: Post[]) => {
+  const userId = posts.map((post) => post.authorId);
+  const users = (
+    await clerkClient.users.getUserList({
+      userId: userId,
+      limit: 110,
+    })
+  ).map(filterUserForClient);
+  return posts.map((post) => {
+    const author = users.find((user) => user.id === post.authorId);
+
+    if (!author) {
+      throw new TRPCError({
+        code: "INTERNAL_SERVER_ERROR",
+        message: `Author for post not found. POST ID: ${post.id}, USER ID: ${post.authorId}`,
+      });
+    }
+    if (!author.username) {
+      if (!author.externalUsername) {
+        throw new TRPCError({
+          code: "INTERNAL_SERVER_ERROR",
+          message: `Author has no GitHub Account: ${author.id}`,
+        });
+      }
+      author.username = author.externalUsername;
+    }
+    return {
+      post,
+      author: {
+        ...author,
+        username: author.username ?? "{username not found)",
+      },
+    };
+  });
+};
 
 export const postRouter = createTRPCRouter({
-  getALL: publicProcedure.query(({ ctx }) => {
-    return ctx.db.post.findMany();
+  getALL: publicProcedure.query(async ({ ctx }) => {
+    const posts = await ctx.db.post.findMany({
+      take: 100,
+      orderBy: [{ createdAt: "desc" }],
+    });
+
+    return addUserDataToPosts(posts);
   }),
 
   // create: publicProcedure
